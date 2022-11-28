de37d26b37b5e6fb048118f46c02da2befa47c84
betty <bettylazar87@gmil.com>
2022-11-28 22:09:03 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import Footer from './Footer';
+import MainContect from './MainContect';
+import NavBar from './NavBar';
+
+function App() {
+  return (
+
+    <div className="App">
+      <NavBar></NavBar>
+      <header className="App-header">
+      <MainContect></MainContect>
+       
+      </header>
+      <Footer></Footer>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/MainContect.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const MainContect = () => {
+  return (
+    <div>
+        <img src="https://picsum.photos/200" alt=" "></img>
+        <img src="https://picsum.photos/201" alt=" "></img>
+        <img src="https://picsum.photos/203" alt=" "></img>
+        <p> BETTY BORENSHTHEIN LAZAR</p>
+    </div>
+  )
+}
+
+export default MainContect
